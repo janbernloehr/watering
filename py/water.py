bc23f6b8da895aa8e580c8efa7b47cbfa4eb5a7b
Jan-Cornelius Molnar <jan.molnar@math.uzh.ch>
2016-05-08 14:53:40 +0200

    web

--- py/water.py
+++ py/water.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
+# wiring stuff
 from time import sleep
-import wiringpi2 as wiringpi
+import wiringpi as wiringpi
+
+# web stuff
+import json
+import falcon
 
 WATERING = 1
 
@@ -17,16 +22,36 @@ LOW = 0
 HIGH = 1
 
 
+def water_all(duration):
+    wiringpi.digitalWrite(WATERING, HIGH)
+
+    sleep(duration)
+
+    wiringpi.digitalWrite(WATERING, LOW)
+
+
 def start_this_app():
     wiringpi.wiringPiSetup()
 
     wiringpi.pinMode(WATERING, OUTPUT)
 
-    wiringpi.digitalWrite(WATERING, HIGH)
 
-    sleep(5)
+class DoWatering:
+    def on_get(self, req, resp, duration):
+        water_all(int(duration))
+        origin = req.get_header('Origin')
+        resp.set_header('Access-Control-Allow-Origin', origin)
+        resp.body = json.dumps({
+            'status': 'watering',
+            'thrust': duration
+        })
+        resp.status = falcon.HTTP_200
 
-    wiringpi.digitalWrite(WATERING, LOW)
 
-
+# Initialize Wiring etc.
 start_this_app()
+
+# Start Web
+app = falcon.API()
+
+app.add_route('/water/{duration}', DoWatering())
