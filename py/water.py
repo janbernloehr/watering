06480bc8734c307fb3df580b968a5b095c729685
Jan-Cornelius Molnar <jan.molnar@math.uzh.ch>
2016-05-10 01:12:06 +0200

    fixed history

--- py/water.py
+++ py/water.py
@@ -38,7 +38,9 @@ def json_serial(obj):
     raise TypeError('Not sure how to serialize %s' % (obj,))
 
 
-def water_all(duration, username):
+def water_all(volume, username):
+    duration = int(float(volume) / s_to_ml_factor)
+
     wiringpi.digitalWrite(WATERING, HIGH)
 
     try:
@@ -47,17 +49,18 @@ def water_all(duration, username):
         db = dataset.connect('sqlite:///mydatabase.db')
 
         db_waterings = db['waterings']
-        db_waterings.insert(dict(waterdate=datetime.utcnow(), user=username, quantity=s_to_ml_factor*duration))
-
+        db_waterings.insert(dict(waterdate=datetime.utcnow(), user=username, quantity=volume))
     finally:
         wiringpi.digitalWrite(WATERING, LOW)
+
+    return duration
 
 
 def record_filling(volume, username):
     db = dataset.connect('sqlite:///mydatabase.db')
 
     db_fillings = db['fillings']
-    db_fillings.insert(dict(waterdate=datetime.utcnow(), user=username, quantity=volume))
+    db_fillings.insert(dict(filldate=datetime.utcnow(), user=username, quantity=volume))
 
 
 def get_history():
@@ -102,12 +105,13 @@ def start_this_app():
 
 
 class DoWatering:
-    def on_get(self, req, resp, duration):
-        water_all(int(duration), 'Jan')
+    def on_get(self, req, resp, volume):
+        duration = water_all(int(volume), 'Jan')
         origin = req.get_header('Origin')
         resp.set_header('Access-Control-Allow-Origin', origin)
         resp.body = json.dumps({
             'action': 'water',
+            'volume': volume,
             'duration': duration
         })
         resp.status = falcon.HTTP_200
@@ -142,6 +146,6 @@ start_this_app()
 # Start Web
 app = falcon.API()
 
-app.add_route('/watering.api/water/{duration}', DoWatering())
+app.add_route('/watering.api/water/{volume}', DoWatering())
 app.add_route('/watering.api/fill/{volume}', RecordFilling())
 app.add_route('/watering.api/history', GetHistory())
